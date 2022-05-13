0f439bb0c48a64f02813ff3cd81601a57a819fd4
Zoeyw26 <98407881+Zoeyw26@users.noreply.github.com>
2022-05-12 22:55:33 -0400

    3

--- studio-do-the-laundry/main.js
+++ studio-do-the-laundry/main.js
@@ -1,12 +1,35 @@
 let inputElement = document.querySelector("#input");
 
-let result = document.querySelector("result");
+let result = document.querySelector("#result");
 
-inputElement.addEventListener("keydown",
+inputElement.addEventListener("keypress",
     function(event) {
-        if (event.key == "enter") {
-            result.innerText = "You typed:" + inputElement.value;
+
+        if (event.key == "type") {
+            let name = inputElement.value;
             inputElement.value = "";
+        }
+        for (let i = 0; i < 4; i++) {
+            let letter = name[i];
+            let selectedCard = document.createElement("img");
+            if (["a", "b", "c", "d", "e"].includes(letter)) {
+                selectedCard.src = shapeCircle;
+            }
+            if (["e", "f", "g", "h", "i"].includes(letter)) {
+                selectedCard.src = devilURL;
+            }
+            if (["h", "i", "j", "k", "l"].includes(letter)) {
+                selectedCard.src = devilURL;
+            }
+            if (["m", "n", "o", "p", "q"].includes(letter)) {
+                selectedCard.src = devilURL;
+            }
+            if (["r", "s", "t", "u", "v"].includes(letter)) {
+                selectedCard.src = devilURL;
+            }
+            if (["w", "x", "y", "z"].includes(letter)) {
+                selectedCard.src = devilURL;
+            }
         }
     }
 )
