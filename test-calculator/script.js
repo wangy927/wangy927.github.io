048e7c3af2a035dec45028821d1ab1053036f4fd
Zoeyw26 <98407881+Zoeyw26@users.noreply.github.com>
2022-05-14 04:05:03 -0400

    !!!

--- test-calculator/script.js
+++ test-calculator/script.js
@@ -45,7 +45,7 @@ class Calculator {
             case '*':
                 computation = prev * current
                 break
-            case '/':
+            case '÷':
                 computation = prev / current
                 break
             default:
